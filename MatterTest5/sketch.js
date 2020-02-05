b864d21a1183e70a74e02d3005f6c0f27dc01b91
795266 <42599270+795266@users.noreply.github.com>
2020-02-05 10:20:24 -0800

    Need to make minimap

--- MatterTest5/sketch.js
+++ MatterTest5/sketch.js
@@ -7,12 +7,20 @@ var Engine = Matter.Engine;
 var Render = Matter.Render;
 var World = Matter.World;
 var Bodies = Matter.Bodies;
+var Body = Matter.Body;
+
+//set up instance variables
+var player;
+var locationX;
+var locationY;
+var radius;
 
 //start program
 function init() {
 
 	//Fetch our canvas
 	var canvas = document.getElementById('cnv');
+	context = canvas.getContext("2d");
 
 	//Setup Matter JS
 	var engine = Engine.create();
@@ -34,27 +42,45 @@ function init() {
 	var obj2 = Bodies.rectangle(-380, 100, 150, 50, { isStatic: true });
 	var obj3 = Bodies.rectangle(-380, -100, 150, 50, { isStatic: true });
 	var obj4 = Bodies.rectangle(380, -100, 150, 50, { isStatic: true });
+	var obj5 = Bodies.rectangle(0, 500, 1000, 50, { isStatic: true });
+	var obj6 = Bodies.rectangle(0, 300, 150, 50, { isStatic: true });
+	var obj7 = Bodies.rectangle(0, 400, 150, 50, { isStatic: true });
+
+
+	//create player
+	this.locationX = 100;
+	this.locationY = 100;
+	this.radius = 30;
+	this.player = Bodies.circle(this.locationX, this.locationY, this.radius, { isStatic: false });
 
 	//Add bodies
-	World.add(world, [obj1, obj2, obj3, obj4]);
+	World.add(world, [obj1, obj2, obj3, obj4, obj5, obj6, player]);
+
+	//Adjust canvas
+	context.translate(400, 0);
 
 	//Run and Render
 	Engine.run(engine);
 	Render.run(render);
 }
 
-function move() {
-	console.log("YYY");S
-	if (move.code == 'KeyW') {
-			context.translate(0, 100);
+//player and screen movement
+function move(event) {
+	console.log(event.code);
+	if (event.key == "ArrowUp") {
+			//context.translate(0, 100);
+			Body.translate(this.player, {x: 0, y: -120})
 	}
-	if (move.code == 'KeyA') {
-			context.translate(100, 0);
+	if (event.key == "ArrowLeft") {
+			context.translate(30, 0);
+			Body.translate(this.player, {x: -30, y: 0})
 	}
-	if (move.code == 'KeyS') {
-			context.translate(0, -100);
+	if (event.key == "ArrowDown") {
+			//context.translate(0, -100);
+			Body.translate(this.player, {x: 0, y: 120})
 	}
-	if (move.code == 'KeyD') {
-			context.translate(-100, 0);
+	if (event.key == "ArrowRight") {
+			context.translate(-30, 0);
+			Body.translate(this.player, {x: 30, y: 0})
 	}
 }
