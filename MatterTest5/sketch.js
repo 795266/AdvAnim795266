8e7847a6ec97b83c9eb5e359197935d2b70fced9
795266 <42599270+795266@users.noreply.github.com>
2020-02-07 10:50:41 -0800

    Need to redo normal map render

--- MatterTest5/sketch.js
+++ MatterTest5/sketch.js
@@ -14,17 +14,30 @@ var player;
 var locationX;
 var locationY;
 var radius;
+var canvas;
+var canvasMap;
+var context;
+var context2;
+var engine;
 
 //start program
 function init() {
 
 	//Fetch our canvas
-	var canvas = document.getElementById('cnv');
+	canvas = document.getElementById('cnv');
 	context = canvas.getContext("2d");
 
+	canvasMap = document.getElementById('cnvMap');
+	context2 = canvasMap.getContext("2d");
+
 	//Setup Matter JS
-	var engine = Engine.create();
+	engine = Engine.create();
 	var world = engine.world;
+	world.bounds.min.x = -2000;
+	world.bounds.min.y = -1500;
+	world.bounds.max.x = 2000;
+	world.bounds.max.y = 1500;
+
 	var render = Render.create({
 		canvas: canvas,
 		engine: engine,
@@ -35,16 +48,19 @@ function init() {
 			wireframes: false,
 			showAngleIndicator: false
 		}
-	});
+
+	})
+
+	//	context.cameraMovement();
 
 	//Create Bodies
-	var obj1 = Bodies.rectangle(380, 100, 150, 50, { isStatic: true });
-	var obj2 = Bodies.rectangle(-380, 100, 150, 50, { isStatic: true });
-	var obj3 = Bodies.rectangle(-380, -100, 150, 50, { isStatic: true });
-	var obj4 = Bodies.rectangle(380, -100, 150, 50, { isStatic: true });
-	var obj5 = Bodies.rectangle(0, 500, 1000, 50, { isStatic: true });
-	var obj6 = Bodies.rectangle(0, 300, 150, 50, { isStatic: true });
-	var obj7 = Bodies.rectangle(0, 400, 150, 50, { isStatic: true });
+	var obj1 = Bodies.rectangle(380, 100, 150, 50, { isStatic: true, width: 150, height: 50 });
+	var obj2 = Bodies.rectangle(-380, 100, 150, 50, { isStatic: true , width: 150, height: 50 });
+	var obj3 = Bodies.rectangle(-380, -100, 150, 50, { isStatic: true , width: 150, height: 50 });
+	var obj4 = Bodies.rectangle(380, -100, 150, 50, { isStatic: true , width: 150, height: 50 });
+	var obj5 = Bodies.rectangle(0, 500, 1000, 50, { isStatic: true , width: 1000, height: 50 });
+	var obj6 = Bodies.rectangle(0, 300, 150, 50, { isStatic: true , width: 150, height: 50 });
+	var obj7 = Bodies.rectangle(0, 400, 150, 50, { isStatic: true , width: 150, height: 50 });
 
 
 	//create player
@@ -62,6 +78,8 @@ function init() {
 	//Run and Render
 	Engine.run(engine);
 	Render.run(render);
+
+	animate();
 }
 
 //player and screen movement
@@ -83,4 +101,54 @@ function move(event) {
 			context.translate(-30, 0);
 			Body.translate(this.player, {x: 30, y: 0})
 	}
+}
+
+function cameraMovement() {
+	this.save();
+	this.translate(player.x - canvas.width / 2, player.y - canvas.height / 2);
+	cameraMovement();
+}
+
+function animate() {
+	context2.fillStyle = "black";
+	context2.fillRect(0,0,canvasMap.width, canvasMap.height);
+
+	var world = engine.world;
+	var worldWidth = world.bounds.max.x - world.bounds.min.x;
+	var worldHeight = world.bounds.max.y - world.bounds.min.y;
+	var xScaleFactor = canvasMap.width/worldWidth;
+	var yScaleFactor = canvasMap.height/worldHeight;
+
+	context2.save();
+	context2.scale(xScaleFactor, yScaleFactor);
+	context2.translate(worldWidth/2, worldHeight/2);
+
+	//go throguh all the bodies in World
+	//for each body check to see what label it is--circle or rectangle
+	context2.beginPath();
+	context2.fillStyle = "gray";
+//	context2.rect()
+
+	for(var i = 0; i < world.bodies.length; i++) {
+		var body = world.bodies[i];
+		if(body.label == "Circle Body") {
+			context2.beginPath();
+			context2.fillStyle = "orange";
+			context2.arc(body.position.x, body.position.y, body.circleRadius, 0, Math.PI*2);
+			context2.fill();
+		}
+		else if(body.label == "Rectangle Body") {
+			context2.beginPath();
+			context2.fillStyle = "orange";
+			context2.rect(body.position.x - body.width/2, body.position.y - body.height/2, body.width, body.height);
+			context2.fill();
+		}
+
+	}
+
+	context2.restore();
+
+
+
+	requestAnimationFrame(animate);
 }
